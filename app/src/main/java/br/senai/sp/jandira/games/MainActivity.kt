4b5cd8175065b73d0160d69215693b3298561dd0
Matheus Alves <matheus.adeoliveira11@gmail.com>
2022-09-23 11:23:32 -0300

    adding binding feature

--- app/src/main/java/br/senai/sp/jandira/games/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/games/MainActivity.kt
@@ -3,9 +3,10 @@ package br.senai.sp.jandira.games
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.senai.sp.jandira.games.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
